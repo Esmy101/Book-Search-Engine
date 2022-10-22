abdbd3fe2fea6ad5ed8074c7033ca53adcb70d1a
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-10-21 22:01:13 -0400

    added the ability to create new users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -23,7 +23,15 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    addUser: async (parent, { username, password }) => {},
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        return res.status(400).json({ message: "Something is wrong!" });
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
     saveBook: async (
       parent,
       { authors, description, title, book_id, link }
