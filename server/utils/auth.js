ff26904bb73816b47d616e9df3785eb6c2aa79f2
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-10-22 17:58:17 -0400

    starting readmre file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function ({ req, res }, next) {
+  authMiddleware: async function ({ req, res }, next) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
     console.log(token);
@@ -17,7 +17,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: "You have no token!" });
+      //return res.status(400).json({ message: "You have no token!" });
     }
 
     // verify token and get user data out of it
@@ -26,7 +26,7 @@ module.exports = {
       req.user = data;
     } catch {
       console.log("Invalid token");
-      return res.status(400).json({ message: "invalid token!" });
+      //return res.status(400).json({ message: "invalid token!" });
     }
 
     // send to next endpoint
